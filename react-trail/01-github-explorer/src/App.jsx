bb450caf7226cbf9222719678da1b533b4d73935
Gleycon Xavier <gleyconxcarlos@gmail.com>
2021-04-28 20:44:43 -0400

    Uploading files

---
+++ react-trail/01-github-explorer/src/App.jsx
@@ -0,0 +1,5 @@
+export function App() {
+  throw new Error('Algo realmente deu errado');
+
+  return <h1>Teste app react</h1>
+}
---
+++ react-trail/01-github-explorer/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopement = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: isDevelopement ? 'development' : 'production',
+  devtool: isDevelopement ? 'eval-source-map' : 'source-map',
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      }
+    ],
+  }
+};
